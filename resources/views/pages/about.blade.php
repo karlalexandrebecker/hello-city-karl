409d916271b22d7a3ac72e3f44e187c103f11e74
Karl Becker <karl.alexandre.becker@gmail.com>
2021-02-22 22:18:11 +0000

    Move static pages to folder pages and remove link from about page

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>About US</title>
+    </head>
+    <body>
+        <p>Built with &hearts; by les Teachers du NET.</p>
+
+        <p><a href="/">Revenir à l apge d'accueil</a></p>
+
+        <footer>
+        <p>&copy; Copyright {{ date('Y') }} &middot;</p>
+        </footer>
+    </body>
+</html>
