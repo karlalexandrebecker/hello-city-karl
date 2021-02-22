91072a9dd283da1d845e31226b80ad92bffdf236
Karl Becker <karl.alexandre.becker@gmail.com>
2021-02-22 22:43:35 +0000

    Remove duplication by using app Layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>About US</title>
-    </head>
-    <body>
+@section('content')
         <p>Built with &hearts; by les Teachers du NET.</p>
 
         <p><a href="/">Revenir à l apge d'accueil</a></p>
-
-        <footer>
-        <p>&copy; Copyright {{ date('Y') }} &middot;</p>
-        </footer>
-    </body>
-</html>
+@endsection
